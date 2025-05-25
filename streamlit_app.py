32f270122fd3785b40927ca82ee6ccc28d7ea314
rajvihu <rajesh.madaraboina@capgemini.com>
2025-05-25 16:47:50 +0530

    Update streamlit_app.py
    uuuu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,6 +11,8 @@ st.write(
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
+# Establish Snowflake connection
+# Make sure your connection is set up correctly in Streamlit secrets or config
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('fruit_name'))
@@ -31,8 +32,8 @@ if ingredients_list:
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')""" 
-    st.write(my_insert_stmt)
-    st.stop()
+    #st.write(my_insert_stmt)
+    #st.stop()
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
